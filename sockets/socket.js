9743e86e0a2434f61cba4d275ccdd8844cb68e9a
Josmar Salvador <josmarmp96@gmail.com>
2021-05-06 15:24:22 -0500

    Cambios sobre bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,18 +1,46 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Queen') );
+bands.addBand( new Band('Bon Jovi') );
+bands.addBand( new Band('Heroes del silencio') );
+bands.addBand( new Band('Metallica') );
+
 //Mensajes de Sockets
 io.on('connection', client => {
 
     console.log('Cliente conectado');
 
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado')
     });
 
-    client.on('mensaje', ( payload ) => {
-        console.log('Mensajee!!!', payload );
+    client.on('vote-band', ( payload ) => {
 
-        io.emit('mensaje', { admin: 'Nuevo mensaje' })
+        bands.voteBand( payload.id );
+        io.emit('active-bands', bands.getBands());
+    });
+
+
+    client.on('add-band', ( payload ) => {
+
+        bands.addBand( new Band( payload.name )  );
+        io.emit('active-bands', bands.getBands());
+    });
+
+    
+    client.on('delete-band', ( payload ) => {
+
+        bands.deleteBand( payload.id  );
+        io.emit('active-bands', bands.getBands());
+        console.log('Banda eliminada')
 
     });
+
 
   });
